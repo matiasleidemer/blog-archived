2c49cec067ab28585f001617d55104f3a986f4a4
Matias Leidemer <matiasleidemer@gmail.com>
2020-12-24 15:00:54 -0300

    Add padding of 8 to both home and blog layouts

--- components/BlogLayout.js
+++ components/BlogLayout.js
@@ -1,25 +1,13 @@
-import NextLink from 'next/link'
-
-const Link = ({ href, text }) => {
-  return (
-    <div className="p-2 md:p-4 text-gray-900 dark:text-gray-100">
-      <NextLink href={href}>
-        <a className="hover:text-gray-600 dark:hover:text-gray-400">{text}</a>
-      </NextLink>
-    </div>
-  )
-}
+import Menu from './Menu'
 
 const Layout = ({ children }) => {
   return (
-    <div className="container mx-auto max-w-2xl p-4 md:p-8">
-      <nav className="flex justify-end">
-        <Link href="/blog" text="Blog" />
-        <Link href="/projects" text="Projects" />
-        <Link href="/" text="Home" />
-      </nav>
-      {children}
-    </div>
+    <>
+      <div className="container mx-auto max-w-2xl p-8">
+        <Menu />
+        <article className="mt-16 mb-8">{children}</article>
+      </div>
+    </>
   )
 }
 
--- components/HomeLayout.js
+++ components/HomeLayout.js
@@ -4,7 +4,7 @@ import Menu from './Menu'
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="container mx-auto max-w-2xl p-4 md:p-8">
+      <div className="container mx-auto max-w-2xl p-8">
         <Menu />
         <Headline />
         {children}
