f004175fc0875eb61ebb69c6fd46109ef3465f36
Matias Leidemer <matiasleidemer@gmail.com>
2021-04-13 18:06:55 -0700

    Update background color to gray-50

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head></Head>
-        <body className="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+        <body className="bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
           <Main />
           <NextScript />
         </body>
