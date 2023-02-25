bf09e11722e716c4507a9a28b6bd19f08fe67d58
Matias Leidemer <matiasleidemer@gmail.com>
2023-02-25 08:08:17 -0300

    Replace google analytics in favor of vercel's

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
+import { Analytics } from '@vercel/analytics/react'
 
 import Head from '../components/Head'
 import Container from '../components/Container'
@@ -11,25 +11,11 @@ import '../styles/dark-code.css'
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-Q6CPX728G2"
-        strategy="afterInteractive"
-      />
-
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-Q6CPX728G2');
-        `}
-      </Script>
-
       <ThemeProvider attribute="class" defaultTheme="system">
         <Head title="Matias Leidemer | Software Developer"></Head>
         <Container>
           <Component {...pageProps} />
+          <Analytics />
         </Container>
       </ThemeProvider>
     </>
